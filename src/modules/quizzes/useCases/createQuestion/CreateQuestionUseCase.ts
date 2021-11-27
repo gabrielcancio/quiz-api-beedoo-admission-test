54c7dc31d78ec3c789492feee2dd22976dac440f
Dev Alitec <alitecgrup@gmail.com>
2021-11-27 11:20:34 -0300

    feat: implementing CreateQuestionUseCase

--- src/modules/quizzes/useCases/createQuestion/CreateQuestionUseCase.ts
+++ src/modules/quizzes/useCases/createQuestion/CreateQuestionUseCase.ts
@@ -1,9 +1,34 @@
+import { AppError } from "../../../../errors/AppError";
 import { IQuestionsRepository } from "../../repositories/interfaces/IQuestionsRepository";
+
+interface IRequest {
+  content: string;
+}
 
 class CreateQuestionUseCase {
   public constructor(private questionRepository: IQuestionsRepository) {}
 
-  public async execute() {}
+  public async execute({ content }: IRequest) {
+    const questionAlreadyExistis = await this.questionRepository.findOne({
+      content,
+    });
+
+    if (this.isContentInvalid(content)) {
+      throw new AppError("Content must be valid!");
+    }
+
+    if (questionAlreadyExistis) {
+      throw new AppError("Question already exists!");
+    }
+
+    const newQuestion = await this.questionRepository.create({ content });
+
+    return newQuestion;
+  }
+
+  private isContentInvalid(content: string) {
+    return content === "" || !content;
+  }
 }
 
 export { CreateQuestionUseCase };
