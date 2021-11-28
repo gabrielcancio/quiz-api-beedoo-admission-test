092984163dab7979cc297e22d12e989577cc6e60
Dev Alitec <alitecgrup@gmail.com>
2021-11-28 00:10:00 -0300

    feat: making integration test to delete question

---
+++ src/modules/quizzes/useCases/deleteQuestion/DeleteQuestionController.spec.ts
@@ -0,0 +1,41 @@
+import request from "supertest"; //eslint-disable-line
+
+import { app } from "../../../../app";
+import { knex } from "../../../../database";
+import { KnexMysqlQuestionsRepository } from "../../repositories/implementations/KnexMysqlQuestionsRepository";
+
+const knexMysqlQuestionsRepository = new KnexMysqlQuestionsRepository();
+
+describe("Delete Question Controller", () => {
+  beforeAll(async () => {
+    await knex.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await knex.migrate.rollback();
+    await knex.destroy();
+  });
+
+  it("Should be able to delete a question", async () => {
+    const { id } = await knexMysqlQuestionsRepository.create({
+      content: "Is Beedoo Awesome?",
+    });
+
+    await request(app).delete(`/questions/${id}`).expect(203);
+    const questions = await knexMysqlQuestionsRepository.listAll();
+
+    expect(questions.length).toBe(0);
+  });
+
+  it("Should not be able to delete a non-existent question", async () => {
+    const invalidId = "invalidId";
+
+    const response = await request(app)
+      .delete(`/questions/${invalidId}`)
+      .expect(400);
+
+    const expectedMessage = "Question must be valid!";
+
+    expect(response.body.message).toBe(expectedMessage);
+  });
+});
