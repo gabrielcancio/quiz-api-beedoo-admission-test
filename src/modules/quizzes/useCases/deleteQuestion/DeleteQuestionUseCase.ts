5673c187b4399af4d02d889e1f5f65e953060a6c
Dev Alitec <alitecgrup@gmail.com>
2021-11-27 23:44:12 -0300

    feat: implementing DeleteQuestionUseCase

--- src/modules/quizzes/useCases/deleteQuestion/DeleteQuestionUseCase.ts
+++ src/modules/quizzes/useCases/deleteQuestion/DeleteQuestionUseCase.ts
@@ -1,9 +1,24 @@
+import { AppError } from "../../../../errors/AppError";
 import { IQuestionsRepository } from "../../repositories/interfaces/IQuestionsRepository";
+
+interface IRequest {
+  id: string;
+}
 
 class DeleteQuestionUseCase {
   public constructor(private questionsRepository: IQuestionsRepository) {}
 
-  public async execute() {}
+  public async execute({ id }: IRequest) {
+    const questionAlreadyExists = await this.questionsRepository.findOne({
+      id,
+    });
+
+    if (!questionAlreadyExists) {
+      throw new AppError("Question must be valid!");
+    }
+
+    await this.questionsRepository.delete({ id });
+  }
 }
 
 export { DeleteQuestionUseCase };
