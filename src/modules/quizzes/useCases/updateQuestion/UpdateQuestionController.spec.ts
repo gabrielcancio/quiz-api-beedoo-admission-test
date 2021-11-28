d9a450f036915364de3172c4ca10739784283c8a
Dev Alitec <alitecgrup@gmail.com>
2021-11-27 23:35:38 -0300

    feat: making integration test to update question

---
+++ src/modules/quizzes/useCases/updateQuestion/UpdateQuestionController.spec.ts
@@ -0,0 +1,70 @@
+import request from "supertest"; //eslint-disable-line
+
+import { app } from "../../../../app";
+import { knex } from "../../../../database";
+import { Question } from "../../models/Question";
+import { KnexMysqlQuestionsRepository } from "../../repositories/implementations/KnexMysqlQuestionsRepository";
+
+let question: Question;
+
+describe("Update Category Controller", () => {
+  beforeAll(async () => {
+    await knex.migrate.latest();
+
+    const knexMysqlQuestionsRepository = new KnexMysqlQuestionsRepository();
+
+    question = await knexMysqlQuestionsRepository.create({
+      content: "Is Beedoo awesome?",
+    });
+  });
+
+  afterAll(async () => {
+    await knex.migrate.rollback();
+    await knex.destroy();
+  });
+
+  it("Should be able to update a question", async () => {
+    const updateData = {
+      content: "Is Beedoo wonderfull?",
+    };
+
+    const response = await request(app)
+      .patch(`/questions/${question.id}`)
+      .send(updateData)
+      .expect(200);
+
+    expect(response.body.id).toBe(question.id);
+    expect(response.body.content).toBe(updateData.content);
+  });
+
+  it("Should not be able to update the content of a non-existent question", async () => {
+    const updateData = {
+      content: "Is Beedoo amazing?",
+    };
+
+    const invalidId = "invalid id!";
+    const response = await request(app)
+      .patch(`/questions/${invalidId}`)
+      .send(updateData)
+      .expect(400);
+
+    const expectedMessage = "Question must be valid!";
+
+    expect(response.body.message).toBe(expectedMessage);
+  });
+
+  it("Should not be able to update a question with invalid content", async () => {
+    const updateData = {
+      content: "",
+    };
+
+    const response = await request(app)
+      .patch(`/questions/${question.id}`)
+      .send(updateData)
+      .expect(400);
+
+    const expectedMessage = "Content must be valid!";
+
+    expect(response.body.message).toBe(expectedMessage);
+  });
+});
