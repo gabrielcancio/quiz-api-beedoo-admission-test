3d30e7063e36ba90999e7e150942f768edd2de9a
Dev Alitec <alitecgrup@gmail.com>
2021-11-27 23:30:56 -0300

    feat: implementing UpdateQuestionUseCase

--- src/modules/quizzes/useCases/updateQuestion/UpdateQuestionUseCase.ts
+++ src/modules/quizzes/useCases/updateQuestion/UpdateQuestionUseCase.ts
@@ -1,9 +1,35 @@
+import { AppError } from "../../../../errors/AppError";
 import { IQuestionsRepository } from "../../repositories/interfaces/IQuestionsRepository";
+
+interface IRequest {
+  id: string;
+  content: string;
+}
 
 class UpdateQuestionUseCase {
   public constructor(private questionsRepository: IQuestionsRepository) {}
 
-  public async execute() {}
+  public async execute({ id, content }: IRequest) {
+    const questionAlreadyExists = await this.questionsRepository.findOne({
+      id,
+    });
+
+    if (!questionAlreadyExists) {
+      throw new AppError("Question must be valid!");
+    }
+
+    if (this.isContentInvalid(content)) {
+      throw new AppError("Content must be valid!");
+    }
+
+    const question = await this.questionsRepository.update({ id, content });
+
+    return question;
+  }
+
+  private isContentInvalid(content: string) {
+    return content === "" || !content;
+  }
 }
 
 export { UpdateQuestionUseCase };
