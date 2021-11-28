fa59f7742be41b86dbdca6e3a91b43cbaf4071dc
Dev Alitec <alitecgrup@gmail.com>
2021-11-27 23:29:17 -0300

    feat: adding the route to list questions

--- src/routes/questions.routes.ts
+++ src/routes/questions.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import { createQuestionController } from "../modules/quizzes/useCases/createQuestion";
+import { listQuestionController } from "../modules/quizzes/useCases/listQuestions";
 
 const questionsRoutes = Router();
 
 questionsRoutes.post("/", (request, response) => {
   return createQuestionController.handle(request, response);
+});
+
+questionsRoutes.get("/", (request, response) => {
+  return listQuestionController.handle(request, response);
 });
 
 export { questionsRoutes };
