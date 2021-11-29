cc3acabbec2de7487e0685a75200c51c4b1b85ee
Dev Alitec <alitecgrup@gmail.com>
2021-11-29 11:40:00 -0300

    feat: implementing UpdateAlternativeUseCase

--- src/modules/quizzes/useCases/updateAlternative/UpdateAlternativeUseCase.ts
+++ src/modules/quizzes/useCases/updateAlternative/UpdateAlternativeUseCase.ts
@@ -1,4 +1,4 @@
-import { Alternative } from "../../models/Alternative";
+import { AppError } from "../../../../errors/AppError";
 import { IAlternativesRepositories } from "../../repositories/interfaces/IAlternativesRepository";
 
 interface IRequest {
@@ -11,8 +11,29 @@ class UpdateAlternativeUseCase {
     private alternativesRepository: IAlternativesRepositories
   ) {}
 
-  public async execute({ id, content }: IRequest): Promise<Alternative> {
-    return {} as Alternative;
+  public async execute({ id, content }: IRequest) {
+    const alternativeAlreadyExists = await this.alternativesRepository.findOne({
+      id,
+    });
+
+    if (!alternativeAlreadyExists) {
+      throw new AppError("Alternative must be valid!");
+    }
+
+    if (this.isContentInvalid(content)) {
+      throw new AppError("Content must be valid!");
+    }
+
+    const alternative = await this.alternativesRepository.update({
+      id,
+      content,
+    });
+
+    return alternative;
+  }
+
+  private isContentInvalid(content: string) {
+    return content === "" || !content;
   }
 }
 
