4fa22166138b988aabeea4e225feff30ec6e0520
Dev Alitec <alitecgrup@gmail.com>
2021-11-29 09:26:30 -0300

    feat: adding routes to list alternatives

--- src/routes/alternatives.routes.ts
+++ src/routes/alternatives.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
 import { createAlternativeController } from "../modules/quizzes/useCases/createAlternative";
+import { listAlternativesController } from "../modules/quizzes/useCases/listAlternatives";
 
 const alternativesRoutes = Router();
 
 alternativesRoutes.post("/", (request, response) => {
   return createAlternativeController.handle(request, response);
+});
+
+alternativesRoutes.get("/", (request, response) => {
+  return listAlternativesController.handle(request, response);
 });
 
 export { alternativesRoutes };
